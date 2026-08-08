9496075c52d821be0008b685dfa5e1e07583e670
Vasil Michorov <vasil.michorov01@outlook.com>
2026-08-08 22:54:11 +0300

    update scripts error handling and styling

--- wifi.hh
+++ wifi.hh
@@ -1,34 +1,69 @@
+const signalColors = [
+  "🔴", // 0
+  "🟠", // 1
+  "🟡", // 2
+  "🟢", // 3
+];
+
 async function Main() {
+  Shell.lock();
   const interfaces = await Networking.Wifi.GetInterfaces();
   const iface = interfaces.find((i) => i.monitor);
-  if (!iface) throw new Error("Error: No interface found!");
+  if (!iface) {
+    println({ text: "Error: No interface found!", color: "red" });
+    Shell.unlock();
+    return;
+  }
 
   const networks = await Networking.Wifi.Scan(iface.name);
-  if (!networks.length) throw new Error("Error: No networks found!");
-  for (let i = 0; i < networks.length; i++) {
-    const network = networks[i];
-    println(
-      `${i + 1}. | ${network.ssid} | ${network.bssid} | Ch:${network.channel} | Signal:${network.signal}`,
-    );
+  if (!networks.length) {
+    println({ text: "Error: No networks found!", color: "red" });
+    Shell.unlock();
+    return;
   }
-  println("0. | Exit");
 
+  printTable(
+    networks.map((n, i) => ({
+      "": i + 1,
+      SSID: n.ssid,
+      BSSID: n.bssid,
+      CHANNEL: n.channel,
+      SIGNAL: signalColors[n.signal],
+    })),
+  );
+
+  Shell.unlock();
   const input = parseInt(await prompt("Select Network: "));
-  if (input === 0) return;
+  Shell.lock();
 
   const target = networks[input - 1];
-  if (!target) throw new Error("Error: Invalid network!");
+  if (!target) {
+    println({ text: "Error: Invalid network!", color: "red" });
+    Shell.unlock();
+    return;
+  }
   println("Deauthenticating Network...");
   await Networking.Wifi.Deauth(iface.name, target.bssid);
   const pcap = await Networking.Wifi.CaptureHandshake(iface.name, target.bssid);
-  if (!pcap) throw new Error("Error: Couldn't capture handshake!");
-  println(`Captured: ${pcap}`);
+  if (!pcap) {
+    println({ text: "Error: Couldn't capture handshake!", color: "red" });
+    Shell.unlock();
+    return;
+  }
 
+  println("Deciphering password...");
   const password = await Crypto.Hashcat.Decrypt(pcap);
-  if (!password) throw new Error("Error: Couldn't decipher password!");
+  if (!password) {
+    println({ text: "Error: Couldn't decipher password!", color: "red" });
+    Shell.unlock();
+    return;
+  }
+
+  println("Connecting to network...");
   await Networking.Wifi.Connect(target.ssid, password);
 
   await FileSystem.Remove(pcap);
+  Shell.unlock();
 }
 
 Main();
