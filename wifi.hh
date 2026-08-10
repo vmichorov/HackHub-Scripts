3ccfb8c7b53d71daf3656f60482b32273ad61750
Vasil Michorov <vasil.michorov01@outlook.com>
2026-08-10 04:03:58 +0300

    update wifi script to only decipher each network's password once and save it in a text file

--- wifi.hh
+++ wifi.hh
@@ -42,27 +42,68 @@ async function Main() {
     Shell.unlock();
     return;
   }
-  println("Deauthenticating Network...");
-  await Networking.Wifi.Deauth(iface.name, target.bssid);
-  const pcap = await Networking.Wifi.CaptureHandshake(iface.name, target.bssid);
-  if (!pcap) {
-    println({ text: "Error: Couldn't capture handshake!", color: "red" });
-    Shell.unlock();
-    return;
-  }
 
-  println("Deciphering password...");
-  const password = await Crypto.Hashcat.Decrypt(pcap);
-  if (!password) {
-    println({ text: "Error: Couldn't decipher password!", color: "red" });
-    Shell.unlock();
-    return;
+  let isPassSaved = false;
+  let password = "";
+  const files = await FileSystem.ReadDir("scripts");
+  const passFile = files!.find((f) => f.name === "passwords");
+  let initialContent = "";
+
+  if (!passFile) {
+    println("Creating passwords file...");
+    await FileSystem.WriteFile("./scripts/passwords.txt", "");
+    isPassSaved = false;
+  } else {
+    println("Searching for password in file...");
+    const content = await FileSystem.ReadFile("./scripts/passwords.txt");
+    initialContent = content;
+    const lines = content.split(/\r?\n/);
+    if (lines.length) {
+      for (let line of lines) {
+        const [name, pass] = line.split(":");
+        if (name === target.ssid) {
+          isPassSaved = true;
+          println("Retrieving password from file...");
+          password = pass;
+          break;
+        }
+      }
+    }
+  }
+  if (!isPassSaved) {
+    println("Password not saved, deauthenticating network...");
+    await Networking.Wifi.Deauth(iface.name, target.bssid);
+    const pcap = await Networking.Wifi.CaptureHandshake(
+      iface.name,
+      target.bssid,
+    );
+    if (!pcap) {
+      println({ text: "Error: Couldn't capture handshake!", color: "red" });
+      Shell.unlock();
+      return;
+    }
+
+    println("Deciphering password...");
+    const decrypt = await Crypto.Hashcat.Decrypt(pcap);
+    if (!decrypt) {
+      println({ text: "Error: Couldn't decipher password!", color: "red" });
+      Shell.unlock();
+      return;
+    }
+    await FileSystem.Remove(pcap);
+    password = decrypt;
+
+    println("Saving password to file...");
+    const newPassFileContent = `${initialContent}\n${target.ssid}:${password}`;
+    await FileSystem.WriteFile(
+      "./scripts/passwords.txt",
+      newPassFileContent.trim(),
+    );
   }
 
   println("Connecting to network...");
   await Networking.Wifi.Connect(target.ssid, password);
 
-  await FileSystem.Remove(pcap);
   Shell.unlock();
 }
 
